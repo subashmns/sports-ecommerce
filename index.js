f6b0a86faa13ddb614bfa5fa7cb2c181bd7f901e
subashebanezer <ricksubash123@gmail.com>
2024-10-23 23:53:25 +0530

    Initial commit

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 const productRoute = require('./product/routes/products.routs')
 require('dotenv').config();
+const cors = require('cors');
 
 const dbUrl = process.env.DATABASE_URL;
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.json()); // to parse json data in request body
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 // app.use('/', (req, res) => {
 //     res.send('Welcome to my API!');
@@ -20,7 +25,7 @@ mongoose.connect(dbUrl)
 .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
-      console.log("Server is running on port 3000 thanks");
+      console.log("Server is running on port 5000 thanks");
     });
   })
   .catch(() => {
