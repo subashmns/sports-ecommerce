22614e789fa91fa8f28ffd51eae34457edcdc18c
subashebanezer <ricksubash123@gmail.com>
2024-10-29 19:34:53 +0530

    New Commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const productRoute = require('./product/routes/products.routs')
-require('dotenv').config();
+const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB  = require('./db/connectDB');
+const authRoutes = require('./customer/routes/auth.route')
 
-const dbUrl = process.env.DATABASE_URL;
-const PORT = 5000;
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse json data in request body
 app.use(express.urlencoded({ extended: false }));
@@ -15,19 +16,12 @@ app.use(cors({
   origin: 'http://localhost:3000'
 }));
 
-// app.use('/', (req, res) => {
-//     res.send('Welcome to my API!');
-// })
+app.use('/products', productRoute);
+app.use('/users', authRoutes);
 
-app.use('/products', productRoute)
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port 5000 thanks");
+});
 
-mongoose.connect(dbUrl)
-.then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log("Server is running on port 5000 thanks");
-    });
-  })
-  .catch(() => {
-    console.log("Failed to connect to MongoDB");
-  });
+    
