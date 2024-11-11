6c34517b2cdaf36138b6136703f87e3d821af063
subashebanezer <ricksubash123@gmail.com>
2024-11-11 10:55:58 +0530

    add cors

--- index.js
+++ index.js
@@ -11,16 +11,13 @@ const sellerProduct = require('./seller/seller.route/seller.route');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-  origin: 'http://localhost:3000', // Allow only your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
+
 
 
 // Routes
--- seller/seller.route/seller.route.js
+++ seller/seller.route/seller.route.js
@@ -7,14 +7,12 @@ const {
     getSellerProductById
 } = require('../seller.controller/seller.product.controller');
 
-const { protect } = require('../tokenVerify/tokenVerify')
-
 const router = express.Router();
 
-router.post('/add',protect, addProduct); // Seller adds a product
-router.get('/products',protect, getSellerProducts); // Seller views their products
-router.put('/product/:productId',protect, updateProduct); // Seller updates a product
-router.delete('/product/:productId',protect, deleteProduct); // Seller deletes a product
-router.get('/products/:productId',protect, getSellerProductById) ; //seller gets its product by id
+router.post('/add', addProduct); // Seller adds a product
+router.get('/products', getSellerProducts); // Seller views their products
+router.put('/product/:productId', updateProduct); // Seller updates a product
+router.delete('/product/:productId', deleteProduct); // Seller deletes a product
+router.get('/products/:productId', getSellerProductById) ; //seller gets its product by id
 
 module.exports = router;
