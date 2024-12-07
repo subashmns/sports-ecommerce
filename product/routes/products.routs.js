e6ff03b72b28de35a2b711d09780865a1dda48e1
subashebanezer <ricksubash123@gmail.com>
2024-12-07 11:08:24 +0530

    preremove commit

--- product/routes/products.routs.js
+++ product/routes/products.routs.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { createProduct, getProducts, getProductsById, updateProduct, deleteProduct } = require('../controllers/product.controller');
 const multer = require('multer');
+// const { protect } = require('../../seller/tokenVerify/tokenVerify');
 
-router.post('/add', multer().array('images', 5), createProduct);
-router.get('/', getProducts);
+router.post('/add', multer().array('images', 5) ,createProduct);
+router.get('/' , getProducts);
 router.get('/:id', getProductsById);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+router.put('/:id' , updateProduct);
+router.delete('/:id' , deleteProduct);
 
 module.exports = router;
